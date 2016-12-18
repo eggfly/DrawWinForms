ee01662ae538444ead7bafbffd20ca2e74b8130d
eggfly <eggfly@qq.com>
2016-12-18 23:45:21 +0800

    抽奖
    Signed-off-by: eggfly <eggfly@qq.com>

---
+++ 抽奖/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace 抽奖
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(246, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抽奖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(658, 372);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(75, 395);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 42);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "抽奖";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 抽奖/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 抽奖
+{
+    public partial class Form1 : Form
+    {
+        private Random mRandom = new Random();
+        private Dictionary<int, bool> mDict = new Dictionary<int, bool>();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = mTotal.ToString();
+        }
+        private int mTotal = 50;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled)
+            {
+                int count = mTotal;
+                bool result = int.TryParse(textBox1.Text, out count);
+                if (!result)
+                {
+                    return;
+                }
+                else
+                {
+                    mTotal = count;
+                    textBox1.Enabled = false;
+                }
+            }
+            lock (mDict)
+            {
+                if (mDict.Count == mTotal)
+                {
+                    button1.Text = "已达到" + mTotal + "人";
+                    return;
+                }
+                while (true)
+                {
+                    var val = mRandom.Next(mTotal);
+                    if (!mDict.ContainsKey(val))
+                    {
+                        mDict.Add(val, true);
+                        richTextBox1.AppendText((val + 1).ToString() + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
